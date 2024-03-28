f34a994553c29fa947eb26eb0b9569d56a60334f
antoine leloup <al@MacBook-Air.local>
2024-03-28 09:59:40 +0100

    modif css et id

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,33 +18,35 @@ const Home = ({ data }) => {
         </div>
       </div>
       <div className="home-card-wrap">
-        {data.offers.map((offer, index) => {
+        {data.offers.map((offer, id) => {
           return (
-            <div className="card-container" key={offer._id}>
-              <div className="card-avatar">
-                <img src={offer.owner.account.avatar.secure_url} />
-                <span>{offer.owner.account.username}</span>
-              </div>
-              <div className="card-pictures">
-                <img
-                  src={offer.product_pictures[0].secure_url}
-                  alt={offer.product_name}
-                />
-              </div>
-              <div className="card-infos">
-                <p>Prix: {offer.product_price} €</p>
-                <ul>
-                  {offer.product_details
-                    .filter((detail) =>
-                      ["TAILLE", "MARQUE"].includes(Object.keys(detail)[0])
-                    )
-                    .map((detail, index) => (
-                      <li key={index}>
-                        {Object.keys(detail)[0]}: {Object.values(detail)[0]}
-                      </li>
-                    ))}
-                </ul>
-              </div>
+            <div className="card-container" key={offer.id}>
+              <Link to={"/offers/" + offer._id}>
+                <div className="card-avatar">
+                  <img src={offer.owner.account.avatar.secure_url} />
+                  <span>{offer.owner.account.username}</span>
+                </div>
+                <div className="card-pictures">
+                  <img
+                    src={offer.product_pictures[0].secure_url}
+                    alt={offer.product_name}
+                  />
+                </div>
+                <div className="card-infos">
+                  <p>Prix: {offer.product_price} €</p>
+                  <ul>
+                    {offer.product_details
+                      .filter((detail) =>
+                        ["TAILLE", "MARQUE"].includes(Object.keys(detail)[0])
+                      )
+                      .map((detail, index) => (
+                        <li key={index}>
+                          {Object.keys(detail)[0]}: {Object.values(detail)[0]}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </Link>
             </div>
           );
         })}
@@ -60,7 +62,6 @@ const Home = ({ data }) => {
           </div>
         </div>
       </div>
-      {/*<Link to="/details">Cliquez ici pour aller ver sla page details</Link>*/}
     </>
   );
 };
--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -3,11 +3,11 @@ import { useParams } from "react-router-dom";
 const Offer = () => {
   //   const params = useParams();
   //   console.log(params.id);
-  const { id } = useParams();
+  const { _id } = useParams();
 
   return (
     <>
-      <h1>Je suis sur la page de l'offre dont l'id est {id}</h1>;
+      <h1>Je suis sur la page de l'offre dont l'id est {_id}</h1>;
     </>
   );
 };
